bf4a535c8f087723b7a4a86c194547d23d82a89c
gem-puzzle <gem.puzzle.acc@gmail.com>
2025-03-25 17:28:45 +0100

    Update Board.tsx

--- app/components/Board.tsx
+++ app/components/Board.tsx
@@ -6,24 +6,22 @@ import { Tile } from '~/components/Tile'
 export function Board () {
   const { tiles, isSolved, isShuffling } = useAtomValue(state)
 
-  const boardStyle = [
+  const board = [
     'size-87.5', 'p-1', 'relative', 'flex', 'flex-wrap', 'gap-1',
-    'rounded-sm', 'bg-blue-50', 'border-5',
-    isSolved && 'border-emerald-500', !isSolved && 'border-rose-500'
+    'rounded-sm', 'bg-blue-50', 'border-5', isSolved && 'border-emerald-500',
+    !isSolved && 'border-rose-500'
   ]
 
-  const overlayStyle = [
-    'size-85', 'absolute', 'top-0', 'start-0', 'backdrop-blur-xs'
+  const overlay = [
+    'size-85', 'absolute', 'top-0', 'start-0', 'z-1', 'backdrop-blur-xs'
   ]
-
-  const spacerStyle = ['size-20']
 
   return (
-    <div className={clsx(boardStyle)}>
-      {isShuffling && <div className={clsx(overlayStyle)} />}
+    <div className={clsx(board)}>
+      {isShuffling && <div className={clsx(overlay)} />}
 
       {tiles.map((tile: number) => tile === 16
-        ? <div key={tile} className={clsx(spacerStyle)} />
+        ? <div key={tile} className="size-20" />
         : <Tile key={tile} num={tile} />
       )}
     </div>
